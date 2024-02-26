b8d7606720f14a012c7b7eb3fcdf67b7c363fb71
Ajay Karthick Senthil Kumar <24907339+iajaykarthick@users.noreply.github.com>
2024-02-26 12:37:05 -0500

    Add setup.py file for project configuration

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='ecg-heart-rhythm-classification',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A machine learning project for classifying heart rhythms using ECG data, focusing on detecting Atrial Fibrillation and other arrhythmias.',
+    author='Ajay Karthick',
+    license='MIT',
+)
